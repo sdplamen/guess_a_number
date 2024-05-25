ad0168e134c4f7259df903fea5c088948837bb06
Plamen Stoilkov <sdplamen@gmail.com>
2024-05-25 16:47:00 +0300

    Update the complexity.

--- guess_a_number.py
+++ guess_a_number.py
@@ -9,7 +9,24 @@ while guess != 6:
     player = int(player)
     if player == computer_number:
         print('You guess it!')
-        break
+        if guess == 1:
+            print('You win with excellent score of 100!')
+        elif guess == 2:
+            print('You win with perfect score of 80!')
+        elif guess == 3:
+            print('You win with good score of 60!')
+        elif guess == 4:
+            print('You win with satisfying of score 40!')
+        elif guess == 5:
+            print('You win with average score of 20!')
+        elif guess == 4:
+            print('You win with score of 10!')
+        print('Play another game [y] for YES or [n] for NO?')
+        again = input()
+        if again == 'y':
+            guess = 0
+        else:
+            break
     elif player > computer_number:
         print('Your number is too high!')
     else:
